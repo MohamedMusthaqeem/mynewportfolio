d8a80770ebaf562bf948d8d14bda81e859abd6de
mohamedmusthaqeem <mohamedmusthaqeem.2101144@srec.ac.in>
2024-05-22 11:01:38 +0530

    Bug Fix link

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/resume.pdf",
+      href: "/Mohamed Musthaqeem MERN.pdf",
       style: "rounded-br-md",
       download: true,
     },
