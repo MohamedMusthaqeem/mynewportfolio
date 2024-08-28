6d96ba57d4fa3b58f3ab3449abdce8f35db2ef8a
MohamedMusthaqeem <mohamedmusthaqeem.2101144@srec.ac.in>
2024-08-28 21:55:55 +0530

    Resume Updated

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/Mohamed Musthaqeem Resume.pdf",
+      href: "/MohamedMusthaqeem Resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
